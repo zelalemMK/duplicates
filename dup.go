506c4175bcff1e485132ad3a07f6d14ef4c8e12e
Zelalem Mekonnen <zelliott33@gmail.com>
2023-06-15 13:43:16 -0500

    learning go and functions

--- dup.go
+++ dup.go
@@ -1,74 +1,94 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"path"
+	"sort"
 )
 
-type FileWithDup struct {
-	FileName string
-	Counts   map[string]int
+var prereq = map[string][]string{
+	"algorithims": {"data structres"},
+	"calculus":    {"linear algebra"},
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
 }
 
 func main() {
-	var counts []FileWithDup
-	files := os.Args[1:]
-
-	if len(files) == 0 {
-		fmt.Fprint(os.Stderr, "No file input\n")
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dub2: %v\n", err)
-				continue
-			}
-			countLines(files, &counts)
-			f.Close()
-		}
-	}
-
-	for _, count := range counts {
-		showFileName := false
-		for line, n := range count.Counts {
-			if n > 1 {
-				fmt.Printf("%d\t%s\n", n, line)
-				showFileName = true
-			}
-		}
-		if showFileName {
-			fmt.Println(count.FileName)
-		}
-
-	}
+	filename, _, _ := fetch("https://www.example.com")
+	fmt.Println(filename)
 }
 
-func countLinesStdin(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
 	}
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.RequestURI)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+	n, err = io.Copy(f, resp.Body)
+	if err != nil {
+		return "", 0, err
+	}
+
+	if closeErr := f.Close(); err == nil {
+		err = closeErr
+	}
+
+	return local, n, err
+
 }
 
-func countLines(filenames []string, c *[]FileWithDup) {
-	for _, arg := range filenames {
-		f, err := os.Open(arg)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "dub2: %v\n", err)
-			continue
+func max(n ...int) int {
+	maxVal := 0
+	for _, i := range n {
+		if i > maxVal {
+			maxVal = i
 		}
+	}
+	return maxVal
+}
 
-		d := FileWithDup{
-			FileName: arg,
-			Counts:   make(map[string]int),
-		}
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
 
-		input := bufio.NewScanner(f)
-		for input.Scan() {
-			d.Counts[input.Text()]++
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
 		}
-		*c = append(*c, d)
 	}
 
+	var keys []string
+	for key := range m {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	visitAll(keys)
+	return order
 }
